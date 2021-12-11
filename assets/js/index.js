957ad7c66af8175623cc92ffc3acbbc1d45c1b1b
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-10 20:23:54 -0800

    stopping point, fetch not working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,29 @@
 var queryString = document.location.search;
-var currentWeatherEl = document.querySelector('#current-weather');
+var currentWeatherEl = document.querySelector("#current-weather");
 
-//get location
-var getLocationName = function() {
-    var queryString = document.location.search;
-    var locationName = queryString.split("=")[1];
-    
-    if (locationName) {
-        currentWeatherEl.textContent = locationName;
-        getLocationWeather(locationName); //not defined yet
-      } else {
-        document.location.replace("./index.html");
-      }
-}
+//get location, needs to be city comma state (only US right now)
+var getLocationName = function () {
+  var queryString = document.location.search;
+  var locationName = queryString.split("=")[1];
+
+  if (locationName) {
+    currentWeatherEl.textContent = locationName;
+    getLocationWeather(locationName); //not defined yet
+    //   } else {
+    //     document.location.replace("./index.html");
+    //   } //endless loop
+  }
+};
 
 //take city and state from input and add to api url
 //error handling for state?
-// var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + City "," + State ",USA&limit=1&appid=8b56b9813dea6c61e27469e004a1b484";
+var getLocationWeather = function (location) {
+  var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=8b56b9813dea6c61e27469e004a1b484";
 
-
-//make fetch request to url
-
+  fetch(apiUrl).then(function (response){
+      if (response.ok) {
+          console.log(response.json());
+      }
+  })
+}
 getLocationName();
