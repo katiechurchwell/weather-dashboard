0e77cddcd188a39c74a8b4178a6ff33444e91e50
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-10 19:51:08 -0800

    pseudocode issue 1

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+//event listener to form submit
+//var state = 
+//var city =
+
+//take city and state from input and add to api url
+//error handling for state?
+// var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + City "," + State ",USA&limit=1&appid=8b56b9813dea6c61e27469e004a1b484";
+
+
+//make fetch request to url
